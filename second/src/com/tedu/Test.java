6f14bb25166624ff995e1a46148c5e8512b808c2
25216 <25216@DESKTOP-P6EG22S>
2020-03-07 16:02:14 +0800

    first

---
+++ second/src/com/tedu/Test.java
@@ -0,0 +1,11 @@
+package com.tedu;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello,myfirstday");
+        System.out.println("hello,myfirstday");
+		System.out.println("hello,myfirstday");
+	}
+
+}
---
+++ second/src/com/tedu/Test3.java
@@ -0,0 +1,11 @@
+package com.tedu;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		System.out.println("hello,myfirstday");
+		System.out.println("hello,mysecondtday");
+		System.out.println("hello,mythirdday");
+	}
+
+}
