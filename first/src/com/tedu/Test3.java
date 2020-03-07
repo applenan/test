22f6b108ffbff23cb35d4054c24c70dbf8e50025
25216 <25216@DESKTOP-P6EG22S>
2020-03-07 15:40:14 +0800

    five

--- first/src/com/tedu/Test3.java
+++ first/src/com/tedu/Test3.java
@@ -4,8 +4,8 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		System.out.println("hello,myfirstday");
-                System.out.println("hello,myfirstday");
-		System.out.println("hello,myfirstday");
+		System.out.println("hello,mysecondtday");
+		System.out.println("hello,mythirdday");
 	}
 
 }
